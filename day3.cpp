969440c7729e70613add410e583549c187795455
Delthii <32238382+Delthii@users.noreply.github.com>
2018-12-03 16:40:41 +0100

    day3

--- day3.cpp
+++ day3.cpp
@@ -12,7 +12,15 @@
 using namespace std;
 
 void partA3(vector<string> input) {
-	bool fabric[1000][1000];
+	auto matrix = new int*[1200];
+	for (int i = 0; i < 1200; i++) {
+		matrix[i] = new int[1200];
+		fill(matrix[i], matrix[i]+1200, 0);
+	}
+
+	int res = 0;
+	int id = 1;
+	unordered_set<int> ids;
 	for (auto s : input) {
 		auto ss = splitString(s, " ");
 		auto dim = splitString(ss[3], "x");
@@ -21,8 +29,20 @@ void partA3(vector<string> input) {
 		auto y = stoi(pos[1]);
 		auto xr = stoi(dim[0]);
 		auto yr = stoi(dim[1]);
-
-
+		ids.insert(id);
+		for (int xi = x; xi < x + xr; xi++) {
+			for (int yi = y; yi < y + yr; yi++) {
+				if (matrix[xi][yi] == 0) matrix[xi][yi] = id;
+				else {
+					ids.erase(matrix[xi][yi]);
+					ids.erase(id);
+				}
+			}
+		}
+		id++;
+	}
+	for (auto i : ids) {
+		cout << i << endl;
 	}
 }
 
@@ -46,5 +66,6 @@ int main() {
 
 	partA3(input);
 	//partB3(input);
+	system("pause");
 	return 0;
 }
