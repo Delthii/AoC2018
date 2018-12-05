94a212e12bec648877dcfff2cac3f1f61b9c11ae
Delthii <32238382+Delthii@users.noreply.github.com>
2018-12-05 17:27:32 +0100

    day5

--- day5.cpp
+++ day5.cpp
@@ -14,20 +14,42 @@ using namespace std;
 
 void partA5(vector<string> input) {
 	string in = input[0];
-	for (int i = 0; i <= in.length(); i++) {
+	string temp = string(in);
+	int m = 100000;
+	
+	in = string(temp);
+	for (int i = 0; i < in.length() - 1; i++) {
 		if (abs(in[i] - in[i + 1]) == 32) {
 			in.erase(i, 2);
-			i--;
-			i--;
+			i -= 2;
+			if (i < -1) i = -1;
 		}
 	}
-	cout << in.size() << endl;
+	if (in.size()  < m) m = in.size();
+	
+	cout << m << endl;
 }
 
 
 
 void partB5(vector<string> input) {
-	
+	string in = input[0];
+	string temp = string(in);
+	int m = 100000;
+	for (char c = 'a'; c <= 'z'; c++) {
+		in = string(temp);
+		in.erase(remove(in.begin(), in.end(), c), in.end());
+		in.erase(remove(in.begin(), in.end(), c - 32), in.end());
+		for (int i = 0; i < in.length() - 1; i++) {
+			if (abs(in[i] - in[i + 1]) == 32) {
+				in.erase(i, 2);
+				i -= 2;
+				if (i < -1) i = -1;
+			}
+		}
+		if (in.size()  < m) m = in.size();
+	}
+	cout << m << endl;
 }
 
 int main() {
@@ -45,7 +67,7 @@ int main() {
 	else cout << "Unable to open file";
 
 	partA5(input);
-	//partB4(input);
+	partB5(input);
 	system("pause");
 	return 0;
 }
