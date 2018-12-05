7d381ab3b394e2ab2dc8d7bed407063bb113deb4
b16petma <malmquist45@gmail.com>
2018-12-05 21:38:25 +0100

    Day6 setup

---
+++ day6.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <vector>
+#include <numeric>
+#include <functional>
+#include <algorithm>
+#include "Helpers.h"
+#include "Stopwatch.h"
+using namespace std;
+
+
+void partA6(vector<string> input) {
+	
+}
+
+
+
+void partB6(vector<string> input) {
+	
+}
+
+int main() {
+	string line;
+	ifstream myfile("in.txt");
+	vector<string> input;
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			input.push_back(line);
+		}
+		myfile.close();
+	}
+	else cout << "Unable to open file";
+
+	partA6(input);
+	//partB6(input);
+	system("pause");
+	return 0;
+}
