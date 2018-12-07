1040397b928ed5b9ed327756c064b7c80076c65c
b16petma <malmquist45@gmail.com>
2018-12-07 16:19:06 +0100

    day7 final

--- day7.cpp
+++ day7.cpp
@@ -27,17 +27,84 @@ void partA7(vector<string> input) {
 	}
 
 	string ans;
-	while (!pre.empty()) {
-
+	
+	while (!nodes.empty()) {
+		unordered_set<char> notready = unordered_set<char>();
+		for (auto p : pre) {
+			if (p.second.size() > 0) {
+				notready.insert(p.first);
+			}
+		}
+		vector<char> cand;
+		for (auto &n : nodes) {
+			if (notready.find(n) == notready.end()) {
+				cand.push_back(n);
+			}
+		}
+		sort(cand.begin(), cand.end());
+		nodes.erase(cand[0]);
+		pre.erase(cand[0]);
+		ans += cand[0];
+		for (auto &p : pre) {
+			p.second.erase(cand[0]);
+		}
 	}
 	cout << ans << endl;
 }
 
 
 
-
 void partB7(vector<string> input) {
-	
+	unordered_map<char, unordered_set<char>> pre;
+	unordered_set<char> nodes;
+	for (auto l : input) {
+		auto ss = splitString(l, " ");
+		pre[ss[7][0]].insert(ss[1][0]);
+		nodes.insert(ss[7][0]);
+		nodes.insert(ss[1][0]);
+	}
+	int workers = 5;
+	int ticks = 0;
+	auto working = unordered_map<char, int>();
+	while (!nodes.empty()) {
+		for (auto it = working.begin(); it != working.end();) {
+			if (it->second <= ticks) {
+				nodes.erase(it->first);
+				pre.erase(it->first);
+				for (auto &p : pre) {
+					p.second.erase(it->first);
+				}
+				it = working.erase(it);
+				workers++;
+			}
+			else {
+				it++;
+			}
+		}
+
+		vector<char> cand;
+		unordered_set<char> notready = unordered_set<char>();
+		for (auto p : pre) {
+			if (p.second.size() > 0) {
+				notready.insert(p.first);
+			}
+		}
+		
+		for (auto &n : nodes) {
+			if (notready.find(n) == notready.end()) {
+				cand.push_back(n);
+			}
+		}
+		sort(cand.begin(), cand.end());
+		for (int i = 0; workers > 0 && i < cand.size(); i++) {
+			if(working.find(cand[i]) == working.end()){
+				working[cand[i]] = cand[i] - 'A' + 60 + 1 + ticks;
+				workers--;
+			}
+		}
+		ticks++;
+	}
+	cout << --ticks << endl;
 }
 
 int main() {
@@ -54,9 +121,9 @@ int main() {
 	}
 	else cout << "Unable to open file";
 
-	partA7(input);
+	//partA7(input);
 
-	//partB7(input);
+	partB7(input);
 	system("pause");
 	return 0;
 }
